668a77420a2e6dab6877661d2bad9aa91175b7ad
teslascience <atgon@mail.ru>
2017-12-16 20:29:34 +0300

    код по состоянию p7-включительно

---
+++ src/restaurants/views.py
@@ -0,0 +1,26 @@
+import random
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+# function based view
+def home(request):
+    html_var = 'f strings'
+    num = random.randint(0, 100000000)
+    html_ = f"""<!DOCTYPE html>
+    <html lang=en>
+
+    <head>
+    </head>
+    <body>
+      <h1>Hello World!</h1>
+      <p>This is {html_var} coming through</p>
+      <p>This is a random number: {num}</p>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_)
+
+def home_new(request):
+    num = random.randint(0, 100000000)
+    return render(request, "base.html", {"html_var": True, "num": num})
