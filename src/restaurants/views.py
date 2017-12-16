ba0cdf1fef7d81cc7d2719595f538d2c7e501653
teslascience <atgon@mail.ru>
2017-12-16 22:26:55 +0300

    код по состоянию p8-включительно

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 # function based view
-def home(request):
+def home_old(request):
     html_var = 'f strings'
     num = random.randint(0, 100000000)
     html_ = f"""<!DOCTYPE html>
@@ -21,6 +21,17 @@ def home(request):
     """
     return HttpResponse(html_)
 
-def home_new(request):
-    num = random.randint(0, 100000000)
-    return render(request, "base.html", {"html_var": True, "num": num})
+def home(request):
+    num = None
+    some_list = [
+    random.randint(0, 100000000),
+    random.randint(0, 100000000),
+    random.randint(0, 100000000)
+    ]
+    condition_bool_item = False
+    if condition_bool_item:
+        num = random.randint(0, 100000000)
+    context = {
+    "num": num,
+    "some_list": some_list}
+    return render(request, "base.html", context)
